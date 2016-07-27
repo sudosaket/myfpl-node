6a70b4b6e8e1e46e9c01a58bff4001d25da64bc8
Saket Mehta <saket.m17@outlook.com>
2016-07-27 19:54:03 +0530

    minor changes

---
+++ scrape.py
@@ -0,0 +1,40 @@
+import mechanize
+import cookielib
+from bs4 import BeautifulSoup
+import html2text
+
+# Browser
+br = mechanize.Browser()
+
+# Cookie Jar
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+# Browser options
+br.set_handle_equiv(True)
+# br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+br.addheaders = [('User-agent', 'Chrome')]
+
+# The site we will navigate into, handling it's session
+# br.open('https://fantasy.premierleague.com')
+#
+# # View available forms
+# for f in br.forms():
+#     print f
+#
+# # Select the second (index one) form (the first form is a search query box)
+# br.select_form(nr=0)
+#
+# # User credentials
+# br.form['login'] = '<username>'
+# br.form['password'] = '<password>'
+#
+# # Login
+# br.submit()
+
+print(br.open('https://fantasy.premierleague.com/a/statistics/total_points').read())
